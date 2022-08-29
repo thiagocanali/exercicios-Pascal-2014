8435abc243e2bc94c1020586744b0043cd48dc50
thiagocanali <thi.98.cwb@hotmail.com>
2022-08-29 10:27:02 -0300

    4

---
+++ Exercicio4(lembrar do ex3).pas
@@ -0,0 +1,22 @@
+Program Pzim ;
+{4.	Faça um programa que receba a idade, altura e peso de cinco pessoas, calcule e mostre:
+a)	A quantidade de pessoas acima dos 50 anos;
+b)	A quantidade de pessoas acima de 1,80m;
+c)	A quantidade de pessoas abaixo de 50kg.}
+var
+idade, contidade, contpeso, contaltura: integer;
+altura, peso: real;
+sair: string;
+Begin
+contidade:=0;
+contpeso:=0;
+contaltura:=0;
+cont:=cont+1;
+for i:= 1 to 5 do
+write('Digite sua idade: ');
+read(idade); 
+write('Digite sua altura: ');
+read(altura);
+write('Digite sua peso: '); 
+
+End.
